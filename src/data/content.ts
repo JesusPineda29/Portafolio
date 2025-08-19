f01ff26468267f2a7a555f9098a0953507143f43
JesusPineda29 <jesuspineda1429@gmail.com>
2025-08-19 14:22:09 -0600

    agregando nuevo proyecto MERN

--- src/data/content.ts
+++ src/data/content.ts
@@ -41,25 +41,34 @@ export const skills: Skill[] = [
 export const projects: Project[] = [
   {
     id: "project1",
-    title: "Administrador de Pacientes de Veterinaria, Fullstack MERN",
-    description: "Para poder iniciar sesión email: email@email.com, contraseña: password. Aplicación fullstack para la gestión de pacientes veterinarios utilizando el stack MERN (MongoDB, Express, React y Node.js) Permite a veterinarios registrar, editar y eliminar pacientes, con autenticación segura y manejo de sesiones. Login con JWT y CRUD completo de pacientes.",
-    image: "MERN.png",
-    tags: ["React", "MongoDB", "Express", "Node.js", "Tailwind CSS", "JavaScript", "HTML", "Responsive Design", "MERN", "JWT", "CRUD", "Autenticación", "Sesiones"],
-    github: "https://github.com/JesusPineda29/apv_backend_deploy",
-    link: "https://fullstack-admin-pacientes-mern.netlify.app"
+    title: "Administrador de Tareas, Fullstack MERN",
+    description: "Administrador de tareas full-stack desarrollado con MERN (MongoDB, Express, React, Node.js). Permite a los usuarios registrarse, iniciar sesión con JWT, restablecer su contraseña, crear proyectos y organizar tareas en estados: Pendiente, En Espera, En Progreso, En Revisión y Completado. Con un backend robusto conectado a MongoDB y un frontend interactivo en React, Uptask ofrece una experiencia completa de gestión de tareas, integrando autenticación segura, manejo de datos y control de estados de forma eficiente.",
+    image: "uptask.png",
+    tags: ["React", "MongoDB", "Express", "Node.js", "Tailwind CSS", "JavaScript", "HTML", "MERN", "JWT", "CRUD", "Autenticación", "Sesiones", "Consumo de API"],
+    github: "https://github.com/JesusPineda29/MERN_backend_upTask",
+    link: "https://mern-frontend-up-task.vercel.app"
   },
   {
     id: "project2",
     title: " Web Profesional para Distribuidora de Materias Primas",
     description: "Esta página fue creada para una distribuidora especializada en el suministro de materias primas para diferentes sectores industriales. Su objetivo es comunicar confianza, profesionalismo y facilitar el contacto directo con clientes potenciales. La estructura está optimizada para destacar los productos y generar conversiones a través de  medios de comunicación y contacto directo por WhatsApp.",
     image: "pat.png",
-    tags: ["React", "TypeScript", "Vite", "Tailwind CSS", "JavaScript", "HTML"],
+    tags: ["Responsive Design", "React", "TypeScript", "Vite", "Tailwind CSS", "JavaScript", "HTML"],
     github: "https://github.com/JesusPineda29/Patyuba",
     link: "https://patyuba.com/"
   },
-
   {
     id: "project3",
+    title: "Administrador de Pacientes de Veterinaria, Fullstack MERN",
+    description: "Para poder iniciar sesión email: email@email.com, contraseña: password. Aplicación fullstack para la gestión de pacientes veterinarios utilizando el stack MERN (MongoDB, Express, React y Node.js) Permite a veterinarios registrar, editar y eliminar pacientes, con autenticación segura y manejo de sesiones. Login con JWT y CRUD completo de pacientes.",
+    image: "MERN.png",
+    tags: ["React", "MongoDB", "Express", "Node.js", "Tailwind CSS", "JavaScript", "HTML", "Responsive Design", "MERN", "JWT", "CRUD", "Autenticación", "Sesiones", "Consumo de API"],
+    github: "https://github.com/JesusPineda29/apv_backend_deploy",
+    link: "https://fullstack-admin-pacientes-mern.netlify.app"
+  },
+
+  {
+    id: "project4",
     title: " video juego Snake",
     description: "Una versión web del clásico Snake, desarrollada completamente desde cero con JavaScript puro y HTML5, sin librerías ni frameworks. Inspirado en la estética de los arcades ochenteros, el juego ofrece una interfaz retro con colores neón, sombras brillantes y efectos visuales que evocan las antiguas pantallas CRT. Es jugable directamente desde el navegador, sin necesidad de instalación.",
     image: "snake.png",
@@ -70,11 +79,11 @@ export const projects: Project[] = [
 
 
   {
-    id: "project4",
+    id: "project5",
     title: "Cotizador Cripto API",
     description: "App web responsiva que permite consultar y comparar precios de criptomonedas en tiempo real. Desarrollada con React, TypeScript y Vite, utiliza Zustand para el manejo de estado y Zod para validar datos obtenidos desde dos APIs externas. Rápida, precisa y con arquitectura limpia.",
     image: "cripto.png",
-    tags: ["React", "TypeScript", "Vite", "Zustand", "Zod", "Consumo de API", "HTML", "CSS", "JavaScript"],
+    tags: ["React", "TypeScript", "Responsive Design", "Vite", "Zustand", "Zod", "Consumo de API", "HTML", "CSS", "JavaScript"],
     github: "https://github.com/johndoe/ecommerce",
     link: "https://cotizador-typescript-react.netlify.app"
   },
@@ -82,44 +91,44 @@ export const projects: Project[] = [
 
 
   {
-    id: "project5",
+    id: "project6",
     title: "Buscador de Clima",
     description: "Una aplicación web intuitiva que permite a los usuarios consultar el clima actual de cualquier ciudad del mundo. Simplemente se selecciona un país, se escribe el nombre de la ciudad y la aplicación muestra la temperatura actual, la máxima, la mínima y la sensación térmica.",
     image: "clima.png",
-    tags: ["React", "TypeScript", "Vite", "HTML", "CSS Modules", "JavaScript", "Zod", "Consumo de API"],
+    tags: ["React", "TypeScript", "Vite", "HTML", "CSS Modules", "Responsive Design", "JavaScript", "Zod", "Consumo de API"],
     github: "https://github.com/JesusPineda29/Buscador-de-Clima-FETCH-API",
     link: "https://buscador-clima-api-ts-react.netlify.app"
   },
 
   {
-    id: "project6",
+    id: "project7",
     title: "Cotizador de Prestamos - React",
     description: "El cotizador de precios permite a los usuarios ingresar los detalles de un préstamo (como el monto, plazo) y calcular el costo total con sus respectivos intereses. El proyecto está diseñado para ser simple, rápido y fácil de usar.",
     image: "prestamo.png",
-    tags: ["HTML", "CSS", "JavaScript", "Bootstrap", "React", "Vite"],
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap", "React", "Vite", "Responsive Design"],
     github: "https://github.com/JesusPineda29/cotizados_prestamos_react",
     link: "https://cotizador-prestamos-react-c7ba9a.netlify.app"
   },
 
   {
-    id: "project7",
+    id: "project8",
     title: "Festival de música",
     description: "Sitio web para festival con diseño moderno y responsivo.",
     image: "festival.png",
-    tags: ["HTML", "SCSS", "JavaScript", "Gulp"],
+    tags: ["HTML", "SCSS", "JavaScript", "Gulp", "Responsive Design"],
     github: "https://github.com/JesusPineda29/festivalMusica",
     link: "https://magical-pie-b0bb57.netlify.app"
   },
 
-  {
-    id: "project8",
-    title: "Blog del Cafe",
-    description: "Sitio web responsivo con HTML, CSS, JavaScript y Bootstrap, enfocado en una experiencia de usuario fluida.",
-    image: "cafe.png",
-    tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
-    github: "https://github.com/johndoe/ecommerce",
-    link: "https://comfy-cactus-ede1e4.netlify.app"
-  }
+  // {
+  //   id: "project9",
+  //   title: "Blog del Cafe",
+  //   description: "Sitio web responsivo con HTML, CSS, JavaScript y Bootstrap, enfocado en una experiencia de usuario fluida.",
+  //   image: "cafe.png",
+  //   tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+  //   github: "https://github.com/johndoe/ecommerce",
+  //   link: "https://comfy-cactus-ede1e4.netlify.app"
+  // }
 ];
 
 export const socialLinks: SocialLink[] = [
